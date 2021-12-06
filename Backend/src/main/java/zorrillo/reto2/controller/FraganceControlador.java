e8c1538c0fb0b8d8452a681da601e21cbb38406c
JairoVarelamt <jairo.varela.mt@correo.usa.edu.co>
2021-12-06 10:31:06 -0500

    CROS

--- Backend/src/main/java/zorrillo/reto2/controller/FraganceControlador.java
+++ Backend/src/main/java/zorrillo/reto2/controller/FraganceControlador.java
@@ -8,6 +8,7 @@ import zorrillo.reto2.service.FraganceServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/fragance")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class FraganceControlador {
     @Autowired
     private FraganceServicio fraganceServicio;
